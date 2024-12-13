259b1a5c3e1ece70994ddec6fa9e0c4817e250a1
Marc TELENA <marc.telena@outlook.fr>
2024-12-13 15:53:52 +0100

    Ajout du fichier config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php 
+   // config.php
+   return [
+      'ia_used' => 'gemini',//openai or gemini
+      'api_key_open_ai' => 'sk-votre-clé-api-ici',
+      'api_key_gemini' => 'AIzaSyAsDtUuyk5SV3Lirrx9LC5c3csfGWYl4VM'
+   ];
