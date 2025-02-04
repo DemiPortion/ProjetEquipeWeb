33fbf3f3b3e93050a0ca963afa5e92ac911367eb
Marc TELENA <marc.telena@outlook.fr>
2025-02-04 19:30:02 +0100

    Bug fixé

--- config.php
+++ config.php
@@ -3,6 +3,6 @@
    return [
       'ia_used' => 'gemini',//openai or gemini
       'api_key_open_ai' => 'sk-votre-clé-api-ici',
-      'api_key_gemini' => 'AIzaSyAsDtUuyk5SV3Lirrx9LC5c3csfGWYl4VM'
+      'api_key_gemini' => 'AIzaSyAsDtUuyk5SV3Lirrx9LC5c3csfGWYl4VM',
       'gemini_model' => 'gemini-1.5-flash-8b'
    ];
